9b01d68b0273909cc42a62549a1ba54c83a0d567
Sujan <everestgaming3@gmail.com>
2020-10-27 11:37:14 +0545

    deleted context folder

--- src/contexts/utils.context.js
+++
@@ -1,9 +0,0 @@
-export const removeTask = (tasks, e) => {
-  const taskName =
-    e.target.previousElementSibling.previousElementSibling.innerText;
-  return tasks.filter((task) => task !== taskName);
-};
-
-export const handleTask = (tasks, task) => {
-  return tasks.concat([task]);
-};
